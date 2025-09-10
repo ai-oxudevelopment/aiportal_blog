5841a7fec36283f1b01a65d829577612ae8bed7e
ALeks ishmanov <cvyatoslavka@gmail.com>
2025-09-10 03:32:10 +0300

    fix tests

--- frontend/test-env.js
+++
@@ -1,4 +0,0 @@
-// Test environment variables
-console.log('NEXT_PUBLIC_STRAPI_API_URL:', process.env.NEXT_PUBLIC_STRAPI_API_URL);
-console.log('NEXT_PUBLIC_STRAPI_API_TOKEN:', process.env.NEXT_PUBLIC_STRAPI_API_TOKEN ? 'SET' : 'NOT SET');
-console.log('NODE_ENV:', process.env.NODE_ENV);
